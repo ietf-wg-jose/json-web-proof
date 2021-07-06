27fbaaed846bcdaa906b72ae11d319ee8de68704
Jeremie Miller <jeremie.miller@gmail.com>
2021-07-06 17:43:00 -0600

    little config file

--- .targets.mk
+++ .targets.mk
@@ -1,4 +1,4 @@
-TARGETS_DRAFTS := draft-jmiller-json-web-proofs
-TARGETS_TAGS :=
+TARGETS_DRAFTS := draft-jmiller-json-web-proofs 
+TARGETS_TAGS := 
 draft-jmiller-json-web-proofs-00.md: draft-jmiller-json-web-proofs.md
 	sed -e 's/draft-jmiller-json-web-proofs-latest/draft-jmiller-json-web-proofs-00/g' $< >$@
