e2707126251f5f10de26964d477766eba22eebc6
David Waite <david@alkaline-solutions.com>
2021-10-13 02:31:08 -0600

    Update .targets.mk

--- .targets.mk
+++ .targets.mk
@@ -1,4 +1,4 @@
-TARGETS_DRAFTS := draft-jmiller-json-web-proof 
+TARGETS_DRAFTS := draft-jmiller-json-web-proof draft-jmiller-json-proof-algorithms 
 TARGETS_TAGS := 
 draft-jmiller-json-web-proof-00.md: draft-jmiller-json-web-proof.md
 	sed -e 's/draft-jmiller-json-web-proof-latest/draft-jmiller-json-web-proof-00/g' $< >$@
