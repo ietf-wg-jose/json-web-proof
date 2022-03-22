449d1f01c6bba58c8f200c39927b2bfb10aa032b
Jeremie Miller <jeremie.miller@gmail.com>
2022-03-21 22:25:55 -0600

    import previous test/scratch code

---
+++ fixtures/fixtures_bbs-x.js
@@ -0,0 +1,119 @@
+const {
+    generateBls12381G2KeyPair,
+    blsSign,
+    blsVerify,
+    blsCreateProof,
+    blsVerifyProof,
+  } = require('@mattrglobal/bbs-signatures');
+
+const { fromKeyLike } = require('jose/jwk/from_key_like');
+const { generateKeyPair } = require('jose/util/generate_key_pair');
+const { calculateThumbprint } = require('jose/jwk/thumbprint');
+const { encode, decode } = require('jose/util/base64url');
+const { readFileSync } = require('fs');
+const { GeneralSign } = require('jose/jws/general/sign');
+const { randomBytes } = require('crypto');
+
+function octet_array(value)
+{
+    if(value instanceof Uint8Array) value = Array.from(value);
+    if(!Array.isArray(value)) value = Array.from(new TextEncoder("utf-8").encode((value)));
+    return JSON.stringify(value).split(',').join(', ')
+}
+
+(async function(){
+    // generate the long-term public key
+    const keyPair = await generateBls12381G2KeyPair();
+
+    const jwk = {};
+    jwk.kty = 'OKP';
+    jwk.crv = 'Bls12381G2';
+    jwk.x = Buffer.from(keyPair.publicKey).toString('base64url');
+    jwk.kid = await calculateThumbprint(jwk);
+    jwk.alg = 'BBS+';
+    jwk.use = 'proof';
+    console.log('JWK:');
+    console.log(JSON.stringify(jwk,0,2));
+    console.log('pub', octet_array(keyPair.publicKey));
+    console.log('priv', octet_array(keyPair.secretKey));
+
+    // generate jwp
+    const jwp = {};
+    const protected = {};
+    protected.iss = 'https://issuer.example';
+    protected.claims = ['family_name', 'given_name', 'email', 'age']
+    protected.typ = 'JPT';
+    protected.alg = 'BBS-X';
+    jwp.protected = encode(JSON.stringify(protected));
+    console.log();
+    console.log('Protected Header:');
+    console.log(JSON.stringify(protected, 0, 2));
+//    console.log('octets:', octet_array(JSON.stringify(protected)));
+//    console.log('encoded:', jwp.protected);
+
+    const protected_buff = Buffer.from(JSON.stringify(protected), 'utf8');
+    jwp.protected = encode(protected_buff);
+    const payloads_buff = [
+        Buffer.from(JSON.stringify('Doe'), 'utf8'),
+        Buffer.from(JSON.stringify('Jay'), 'utf8'),
+        Buffer.from(JSON.stringify('jaydoe@example.org'), 'utf8'),
+        Buffer.from(JSON.stringify(42), 'utf8')
+    ];
+    jwp.payloads = payloads_buff.map(encode);
+    
+    let messages = [];
+    messages.push(Uint8Array.from(protected_buff));
+    messages = messages.concat(payloads_buff.map((item)=>Uint8Array.from(item)));
+    const signature = await blsSign({
+        keyPair,
+        messages
+    });
+
+    let x = messages.map((item)=>Array.from(item))
+    console.log('messages',JSON.stringify(x).split(',').join(', '))
+    console.log('signature', octet_array(signature));
+  
+    jwp.proof = encode(signature);
+    console.log()
+    console.log('JSON Serialization:');
+    console.log(JSON.stringify(jwp,0,2));
+
+    const serialized = [];
+    serialized.push(encode(JSON.stringify(jwp.protected)));
+    serialized.push(jwp.payloads.join('~'));
+    serialized.push(jwp.proof);
+    console.log()
+    console.log('Compact Serialization:');
+    console.log(serialized.join('.'));
+
+    // generate a proof with selective disclosure of only the name and age
+    const nonce = randomBytes(32);
+    console.log('nonce', octet_array(nonce));
+
+    const proof = await blsCreateProof({
+        signature,
+        publicKey: keyPair.publicKey,
+        messages,
+        nonce: Uint8Array.from(nonce),
+        revealed: [0,2,4],
+    });
+    console.log('proof', octet_array(proof));
+
+    jwp.payloads[0] = null;
+    jwp.payloads[2] = null;
+    jwp.proof = encode(proof);
+    console.log('JSON Serialization:');
+    console.log(JSON.stringify(jwp,0,2));
+
+    jwp.payloads[0] = '';
+    jwp.payloads[2] = '';
+    const presentation = [];
+    presentation.push(encode(JSON.stringify(jwp.protected)));
+    presentation.push(jwp.payloads.join('~'));
+    presentation.push(encode(proof));
+    console.log()
+    console.log('Compact Presentation:');
+    console.log(presentation.join('.'));
+      
+
+})();
---
+++ fixtures/fixtures_su-es256.js
@@ -0,0 +1,158 @@
+
+const { fromKeyLike } = require('jose/jwk/from_key_like');
+const { generateKeyPair } = require('jose/util/generate_key_pair');
+const { calculateThumbprint } = require('jose/jwk/thumbprint');
+const { encode, decode } = require('jose/util/base64url');
+const { readFileSync } = require('fs');
+const { GeneralSign } = require('jose/jws/general/sign');
+const { parseJwk } = require('jose/jwk/parse');
+
+const stable_key = {
+    "kty": "EC",
+    "x": "ONebN43-G5DOwZX6jCVpEYEe0bYd5WDybXAG0sL3iDA",
+    "y": "b0MHuYfSxu3Pj4DAyDXabAc0mPjpB1worEpr3yyrft4",
+    "crv": "P-256",
+    "d": "jnE0-9YvxQtLJEKcyUHU6HQ3Y9nSDnh0NstYJFn7RuI"
+};
+
+const ephemeral_key = {
+    "kty": "EC",
+    "x": "acbIQiuMs3i8_uszEjJ2tpTtRM4EU3yz91PH6CdH2V0",
+    "y": "_KcyLj9vWMptnmKtm46GqDz8wf74I5LKgrl2GzH3nSE",
+    "crv": "P-256",
+    "d": "Yfg5t1lo9T36QJJkrX0XiPd8Bj0Z6dt3zNqGIkyuOFc"
+}
+
+// use jose wrapper
+async function sign_payload(payload, key){
+    const sig = new GeneralSign(decode(payload));
+    sig.addSignature(key).setProtectedHeader({'alg':'ES256'});
+    const jws = await sig.sign();
+    return jws.signatures[0].signature;
+}
+
+function octet_array(value)
+{
+    if(!Array.isArray(value)) value = Array.from(new TextEncoder("utf-8").encode((value)));
+    return JSON.stringify(value).split(',').join(', ')
+}
+
+(async function(){
+    // generate the long-term public key
+//    const signer = await generateKeyPair('ES256');
+    const stable = {};
+    stable.privateKey = await parseJwk(stable_key, 'ES256');
+    delete stable_key.d;
+    stable.publicKey = await parseJwk(stable_key, 'ES256');
+    const jwk = await fromKeyLike(stable.privateKey);
+//    jwk.kid = await calculateThumbprint(jwk);
+    console.log('Issuer JWK:');
+    console.log(JSON.stringify(jwk,0,2));
+
+    // generate the ephemeral key
+//    const ephemeral = await generateKeyPair('ES256');
+    const ephemeral = {};
+    ephemeral.privateKey = await parseJwk(ephemeral_key, 'ES256');
+    delete ephemeral_key.d;
+    ephemeral.publicKey = await parseJwk(ephemeral_key, 'ES256');
+    const ejwk = await fromKeyLike(ephemeral.publicKey);
+    const ejwk_private = await fromKeyLike(ephemeral.privateKey);
+    console.log();
+    console.log('Ephemeral JWK:');
+    console.log(JSON.stringify(ejwk_private,0,2));
+
+    // storage as we build up
+    const sigs = [];
+    const jwp = {payloads:[]};
+
+    // create the protected header first
+    const protected = {};
+//    protected.kid = jwk.kid;
+    protected.iss = 'https://issuer.tld';
+    protected.claims = ['family_name', 'given_name', 'email', 'age']
+    protected.typ = 'JPT';
+    protected.proof_jwk = ejwk;
+    protected.alg = 'SU-ES256';
+    jwp.protected = encode(JSON.stringify(protected));
+    console.log();
+    console.log('Protected Header:');
+    console.log(JSON.stringify(protected, 0, 2));
+    console.log('octets:', octet_array(JSON.stringify(protected)));
+    console.log('encoded:', jwp.protected);
+
+    // encode/sign the protected header w/ the stable key
+    signature = await sign_payload(jwp.protected, stable.privateKey);
+    sigs.push(signature);
+    console.log('protected sig:', signature);
+    console.log('octets:', octet_array(Array.from(decode(signature))));
+    
+    // encode/sign each payload
+    payload = JSON.stringify('Doe');
+    encoded = encode(payload);
+    jwp.payloads.push(encoded);
+    signature = await sign_payload(encoded, ephemeral.privateKey);
+    sigs.push(signature);
+    console.log();
+    console.log('payload:', encoded);
+    console.log('octets:', octet_array(payload));
+    console.log('sig:', signature);
+    console.log('octets:', octet_array(Array.from(decode(signature))));
+
+    payload = JSON.stringify('Jay');
+    encoded = encode(payload);
+    jwp.payloads.push(encoded);
+    signature = await sign_payload(encoded, ephemeral.privateKey);
+    sigs.push(signature);
+    console.log();
+    console.log('payload:', encoded);
+    console.log('octets:', octet_array(payload));
+    console.log('sig:', signature);
+    console.log('octets:', octet_array(Array.from(decode(signature))));
+
+    payload = JSON.stringify('jaydoe@example.org');
+    encoded = encode(payload);
+    jwp.payloads.push(encoded);
+    signature = await sign_payload(encoded, ephemeral.privateKey);
+    sigs.push(signature);
+    console.log();
+    console.log('payload:', encoded);
+    console.log('octets:', octet_array(payload));
+    console.log('sig:', signature);
+    console.log('octets:', octet_array(Array.from(decode(signature))));
+
+    payload = JSON.stringify(42);
+    encoded = encode(payload);
+    jwp.payloads.push(encoded);
+    signature = await sign_payload(encoded, ephemeral.privateKey);
+    sigs.push(signature);
+    console.log();
+    console.log('payload:', encoded);
+    console.log('octets:', octet_array(payload));
+    console.log('sig:', signature);
+    console.log('octets:', octet_array(Array.from(decode(signature))));
+
+    // merge final signature
+    let final = Buffer.from([]);
+    for(sig of sigs)
+    {
+        final = Buffer.concat([final, decode(sig)]);
+    }
+    jwp.proof = encode(final);
+    console.log();
+    console.log('final:', encode(final));
+    console.log('octets:', octet_array(Array.from(final)));
+
+    console.log();
+    console.log('JSON Serialization:');
+    console.log(JSON.stringify(jwp,0,2));
+
+
+    const serialized = [];
+    serialized.push(jwp.protected);
+    serialized.push(jwp.payloads.join('~'));
+    serialized.push(jwp.proof);
+    console.log();
+    console.log('Compact Serialization:');
+    console.log(serialized.join('.'));
+    
+})();
